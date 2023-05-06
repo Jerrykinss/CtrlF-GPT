c95b02aeb04e15139bbf3e7a4f23cf6d306f9110
Jerry kang <jkang365@uwo.ca>
2023-05-06 01:03:45 -0400

    First Commit

---
+++ src/js/content.js
@@ -0,0 +1,291 @@
+/*** CONSTANTS ***/
+var ELEMENT_NODE_TYPE = 1;
+var TEXT_NODE_TYPE = 3;
+var UNEXPANDABLE = /(script|style|svg|audio|canvas|figure|video|select|input|textarea)/i;
+var HIGHLIGHT_TAG = 'highlight-tag';
+var HIGHLIGHT_CLASS = 'highlighted';
+var SELECTED_CLASS = 'selected';
+var DEFAULT_MAX_RESULTS = 500;
+var DEFAULT_HIGHLIGHT_COLOR = '#ffff00';
+var DEFAULT_SELECTED_COLOR = '#2547da';
+var DEFAULT_TEXT_COLOR = '#000000';
+var DEFAULT_CASE_INSENSITIVE = false;
+/*** CONSTANTS ***/
+
+/*** VARIABLES ***/
+var searchInfo;
+/*** VARIABLES ***/
+                     
+/*** LIBRARY FUNCTIONS ***/
+Element.prototype.documentOffsetTop = function () {
+  return this.offsetTop + ( this.offsetParent ? this.offsetParent.documentOffsetTop() : 0 );
+};
+Element.prototype.visible = function() {
+    return (!window.getComputedStyle(this) || window.getComputedStyle(this).getPropertyValue('display') == '' || 
+           window.getComputedStyle(this).getPropertyValue('display') != 'none')
+}
+/*** LIBRARY FUNCTIONS ***/
+
+
+/*** FUNCTIONS ***/
+/* Initialize search information for this tab */
+function initSearchInfo(pattern) {
+  var pattern = typeof pattern !== 'undefined' ? pattern : '';
+  searchInfo = {
+    regexString : pattern,
+    selectedIndex : 0,
+    highlightedNodes : [],
+    length : 0
+  }
+}
+
+/* Send message with search information for this tab */
+function returnSearchInfo(cause) {
+  chrome.runtime.sendMessage({
+    'message' : 'returnSearchInfo',
+    'regexString' : searchInfo.regexString,
+    'currentSelection' : searchInfo.selectedIndex,
+    'numResults' : searchInfo.length,
+    'cause' : cause
+  });
+}
+
+/* Check if the given node is a text node */
+function isTextNode(node) {
+  return node && node.nodeType === TEXT_NODE_TYPE;
+}
+
+/* Check if the given node is an expandable node that will yield text nodes */
+function isExpandable(node) {
+  return node && node.nodeType === ELEMENT_NODE_TYPE && node.childNodes && 
+         !UNEXPANDABLE.test(node.tagName) && node.visible();
+}
+
+/* Highlight all text that matches regex */
+function highlight(regex, regexString, highlightColor, selectedColor, textColor, maxResults) {
+  let regexList = [regexString];
+  let options = {
+    method: 'GET',
+    headers: { 'x-api-key': '/cUeYXJRNyCaMH7FDK5G9w==Xh7I4HJRi9GstE91' }
+  }
+  let url = 'https://api.api-ninjas.com/v1/thesaurus?word=' + regexString;
+  fetch(url,options)
+        .then(res => res.json()) // parse response as JSON
+        .then(data => {
+          if (data && data.synonyms) {
+            regexList = regexList.concat(data.synonyms);
+            console.log(regexList);
+          }
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+        });
+  function highlightRecursive(node) {
+    if(searchInfo.length >= maxResults){
+      return;
+    }
+    if (isTextNode(node)) {
+      for (const reg of regexList){
+        var index = node.data.search(reg);
+        if (index >= 0 && node.data.length > 0) {
+          var matchedText = node.data.match(reg)[0];
+          var matchedTextNode = node.splitText(index);
+          matchedTextNode.splitText(matchedText.length);
+          var spanNode = document.createElement(HIGHLIGHT_TAG); 
+          spanNode.className = HIGHLIGHT_CLASS;
+          spanNode.style.backgroundColor = highlightColor;
+          spanNode.style.color = textColor;
+          spanNode.appendChild(matchedTextNode.cloneNode(true));
+          matchedTextNode.parentNode.replaceChild(spanNode, matchedTextNode);
+          searchInfo.highlightedNodes.push(spanNode);
+          searchInfo.length += 1;
+          return 1;
+        }
+      }
+    } else if (isExpandable(node)) {
+        var children = node.childNodes;
+        for (var i = 0; i < children.length; ++i) {
+          var child = children[i];
+          i += highlightRecursive(child);
+        }
+    }
+    return 0;
+  }
+  highlightRecursive(document.getElementsByTagName('body')[0]);
+};
+
+/* Remove all highlights from page */
+function removeHighlight() {
+  while (node = document.body.querySelector(HIGHLIGHT_TAG + '.' + HIGHLIGHT_CLASS)) {
+    node.outerHTML = node.innerHTML;
+  }
+    while (node = document.body.querySelector(HIGHLIGHT_TAG + '.' + SELECTED_CLASS)) {
+    node.outerHTML = node.innerHTML;
+  }
+};
+
+/* Scroll page to given element */
+function scrollToElement(element) {
+    element.scrollIntoView(); 
+    var top = element.documentOffsetTop() - ( window.innerHeight / 2 );
+    window.scrollTo( 0, Math.max(top, window.pageYOffset - (window.innerHeight/2)));
+}
+
+/* Select first regex match on page */
+function selectFirstNode(selectedColor) {
+  var length =  searchInfo.length;
+  if(length > 0) {
+    searchInfo.highlightedNodes[0].className = SELECTED_CLASS;
+    searchInfo.highlightedNodes[0].style.backgroundColor = selectedColor;
+    parentNode = searchInfo.highlightedNodes[0].parentNode;
+    if (parentNode.nodeType === 1) {
+      parentNode.focus();
+    } else if (parentNode.parentNode.nodeType == 1) {
+      parentNode.parentNode.focus();
+    }
+    scrollToElement(searchInfo.highlightedNodes[0]);
+  }
+}
+
+/* Helper for selecting a regex matched element */
+function selectNode(highlightedColor, selectedColor, getNext) {
+  var length = searchInfo.length;
+  if(length > 0) {
+    searchInfo.highlightedNodes[searchInfo.selectedIndex].className = HIGHLIGHT_CLASS;
+    searchInfo.highlightedNodes[searchInfo.selectedIndex].style.backgroundColor = highlightedColor;
+      if(getNext) {
+        if(searchInfo.selectedIndex === length - 1) {
+          searchInfo.selectedIndex = 0; 
+        } else {
+          searchInfo.selectedIndex += 1;
+        }
+      } else {
+        if(searchInfo.selectedIndex === 0) {
+          searchInfo.selectedIndex = length - 1; 
+        } else {
+          searchInfo.selectedIndex -= 1;
+        }
+      }
+    searchInfo.highlightedNodes[searchInfo.selectedIndex].className = SELECTED_CLASS;
+    searchInfo.highlightedNodes[searchInfo.selectedIndex].style.backgroundColor = selectedColor;
+    parentNode = searchInfo.highlightedNodes[searchInfo.selectedIndex].parentNode;
+    if (parentNode.nodeType === 1) {
+      parentNode.focus();
+    } else if (parentNode.parentNode.nodeType == 1) {
+      parentNode.parentNode.focus();
+    }
+    returnSearchInfo('selectNode');
+    scrollToElement(searchInfo.highlightedNodes[searchInfo.selectedIndex]);
+  }
+}
+/* Forward cycle through regex matched elements */
+function selectNextNode(highlightedColor, selectedColor) {
+  selectNode(highlightedColor, selectedColor, true); 
+}
+
+/* Backward cycle through regex matched elements */
+function selectPrevNode(highlightedColor, selectedColor) {
+  selectNode(highlightedColor, selectedColor, false);
+}
+
+/* Validate that a given pattern string is a valid regex */
+function validateRegex(pattern) {
+  try{
+    var regex = new RegExp(pattern);
+    return regex;
+  } catch(e) {
+    return false;
+  }
+}
+
+/* Find and highlight regex matches in web page from a given regex string or pattern */
+function search(regexString, configurationChanged) {
+  var regex = validateRegex(regexString);
+  if (regex && regexString != '' && (configurationChanged || regexString !== searchInfo.regexString)) { // new valid regex string
+    removeHighlight();
+    chrome.storage.local.get({
+      'highlightColor' : DEFAULT_HIGHLIGHT_COLOR,
+      'selectedColor' : DEFAULT_SELECTED_COLOR,
+      'textColor' : DEFAULT_TEXT_COLOR,
+      'maxResults' : DEFAULT_MAX_RESULTS,
+      'caseInsensitive' : DEFAULT_CASE_INSENSITIVE}, 
+      function(result) {
+        initSearchInfo(regexString);
+        if(result.caseInsensitive){
+          regex = new RegExp(regexString, 'i');
+        }
+        highlight(regex, regexString, result.highlightColor, result.selectedColor, result.textColor, result.maxResults);
+        selectFirstNode(result.selectedColor);
+        returnSearchInfo('search');
+      }
+    );
+  } else if (regex && regexString != '' && regexString === searchInfo.regexString) { // elements are already highlighted
+    chrome.storage.local.get({
+      'highlightColor' : DEFAULT_HIGHLIGHT_COLOR,
+      'selectedColor' : DEFAULT_SELECTED_COLOR}, 
+      function(result) {
+        selectNextNode(result.highlightColor, result.selectedColor);
+      }
+    );
+  } else { // blank string or invalid regex
+    removeHighlight();
+    initSearchInfo(regexString);
+    returnSearchInfo('search');
+  }
+}
+/*** FUNCTIONS ***/
+
+/*** LISTENERS ***/
+/* Received search message, find regex matches */
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if ('search' == request.message) {
+    search(request.regexString, request.configurationChanged);
+  }
+  /* Received selectNextNode message, select next regex match */
+  else if ('selectNextNode' == request.message) {
+    chrome.storage.local.get({
+      'highlightColor' : DEFAULT_HIGHLIGHT_COLOR,
+      'selectedColor' : DEFAULT_SELECTED_COLOR
+      }, 
+      function(result) {
+        selectNextNode(result.highlightColor, result.selectedColor);
+      }
+    );
+  }
+  /* Received selectPrevNode message, select previous regex match */
+  else if ('selectPrevNode' == request.message) {
+    chrome.storage.local.get({
+      'highlightColor' : DEFAULT_HIGHLIGHT_COLOR,
+      'selectedColor' : DEFAULT_SELECTED_COLOR
+      }, 
+      function(result) {
+        selectPrevNode(result.highlightColor, result.selectedColor);
+      }
+    );
+  }
+  else if ('copyToClipboard' == request.message) {
+    var clipboardHelper = document.createElement('textarea');
+    try {
+      var text = searchInfo.highlightedNodes.map(function (n) {
+        return n.innerText;
+      }).join('\n');
+      clipboardHelper.appendChild(document.createTextNode(text));
+      document.body.appendChild(clipboardHelper);
+      clipboardHelper.select();
+      document.execCommand('copy');
+    } finally {
+      document.body.removeChild(clipboardHelper);
+    }
+  }
+  /* Received getSearchInfo message, return search information for this tab */
+  else if ('getSearchInfo' == request.message) {
+    sendResponse({message: "I'm alive!"});
+    returnSearchInfo('getSearchInfo');
+  }
+});
+/*** LISTENERS ***/
+
+
+/*** INIT ***/
+initSearchInfo();
+/*** INIT ***/
---
+++ src/js/popup.js
@@ -0,0 +1,340 @@
+/*** CONSTANTS ***/
+var DEFAULT_INSTANT_RESULTS = true;
+var ERROR_COLOR = '#ff8989';
+var WHITE_COLOR = '#ffffff';
+var ERROR_TEXT = "Content script was not loaded. Are you currently in the Chrome Web Store or in a chrome:// page? If you are, content scripts won't work here. If not, please wait for the page to finish loading or refresh the page.";
+var SHOW_HISTORY_TITLE = "Show search history";
+var HIDE_HISTORY_TITLE = "Hide search history";
+var ENABLE_CASE_INSENSITIVE_TITLE = "Enable case insensitive search";
+var DISABLE_CASE_INSENSITIVE_TITLE = "Disable case insensitive search";
+var HISTORY_IS_EMPTY_TEXT = "Search history is empty.";
+var CLEAR_ALL_HISTORY_TEXT = "Clear History";
+var DEFAULT_CASE_INSENSITIVE = false;
+var MAX_HISTORY_LENGTH = 30;
+/*** CONSTANTS ***/
+
+/*** VARIABLES ***/
+var sentInput = false;
+var processingKey = false;
+var searchHistory = null;
+var maxHistoryLength = MAX_HISTORY_LENGTH;
+/*** VARIABLES ***/
+
+/*** FUNCTIONS ***/
+
+/* Send message to content script of tab to select next result */
+function selectNext(){
+  chrome.tabs.query({
+    'active': true,
+    'currentWindow': true
+  },
+  function(tabs) {
+    if ('undefined' != typeof tabs[0].id && tabs[0].id) {
+      chrome.tabs.sendMessage(tabs[0].id, {
+        'message' : 'selectNextNode'
+      });
+    }
+  });
+}
+
+/* Send message to content script of tab to select previous result */
+function selectPrev(){
+  chrome.tabs.query({
+    'active': true,
+    'currentWindow': true
+  },
+  function(tabs) {
+    if ('undefined' != typeof tabs[0].id && tabs[0].id) {
+      chrome.tabs.sendMessage(tabs[0].id, {
+        'message' : 'selectPrevNode'
+      });
+    }
+  });
+}
+
+/* Send message to pass input string to content script of tab to find and highlight regex matches */
+function passInputToContentScript(){
+  passInputToContentScript(false);
+}
+
+function passInputToContentScript(configurationChanged){
+  if (!processingKey) {
+    var regexString = document.getElementById('inputRegex').value;
+    document.getElementById('inputRegex').style.backgroundColor = WHITE_COLOR;
+    chrome.tabs.query(
+      { 'active': true, 'currentWindow': true },
+      function(tabs) {
+        if ('undefined' != typeof tabs[0].id && tabs[0].id) {
+          processingKey = true;
+          chrome.tabs.sendMessage(tabs[0].id, {
+            'message' : 'search',
+            'regexString' : regexString,
+            'configurationChanged' : configurationChanged,
+            'getNext' : true
+          });
+          sentInput = true;
+        }
+      }
+    );
+  }
+}
+
+function createHistoryLineElement(text) {
+  var deleteEntrySpan = document.createElement('span');
+  deleteEntrySpan.className = 'historyDeleteEntry'
+  deleteEntrySpan.textContent = '\u2715';
+  deleteEntrySpan.addEventListener('click', function() {
+    for (var i = searchHistory.length - 1; i >= 0; i--) {
+      if (searchHistory[i] == text) {
+        searchHistory.splice(i, 1);
+      }
+    }
+    chrome.storage.local.set({searchHistory: searchHistory});
+    updateHistoryDiv();
+  });
+  var linkSpan = document.createElement('span');
+  linkSpan.className = 'historyLink'
+  linkSpan.textContent = text;
+  linkSpan.addEventListener('click', function() {
+    if (document.getElementById('inputRegex').value !== text) {
+      document.getElementById('inputRegex').value = text;
+      passInputToContentScript();
+      document.getElementById('inputRegex').focus();
+    }
+  });
+  var lineDiv = document.createElement('div');
+  lineDiv.appendChild(deleteEntrySpan);
+  lineDiv.appendChild(linkSpan);
+  return lineDiv;
+}
+
+function updateHistoryDiv() {
+  var historyDiv = document.getElementById('history');
+  if (historyDiv) {
+    historyDiv.innerHTML = '';
+    if (searchHistory.length == 0) {
+      var span = document.createElement('span');
+      span.className = 'historyIsEmptyMessage';
+      span.textContent = HISTORY_IS_EMPTY_TEXT;
+      historyDiv.appendChild(span);
+    } else {
+      for (var i = searchHistory.length - 1; i >= 0; i--) {
+        historyDiv.appendChild(createHistoryLineElement(searchHistory[i]));
+      }
+      var clearButton = document.createElement('a');
+      clearButton.href = '#';
+      clearButton.type = 'button';
+      clearButton.textContent = CLEAR_ALL_HISTORY_TEXT;
+      clearButton.className = 'clearHistoryButton';
+      clearButton.addEventListener('click', clearSearchHistory);
+      historyDiv.appendChild(clearButton);
+    }
+  }
+}
+
+function addToHistory(regex) {
+  if (regex && searchHistory !== null) {
+    if (searchHistory.length == 0 || searchHistory[searchHistory.length - 1] != regex) {
+      searchHistory.push(regex);
+    }
+    for (var i = searchHistory.length - 2; i >= 0; i--) {
+      if (searchHistory[i] == regex) {
+        searchHistory.splice(i, 1);
+      }
+    }
+    if (searchHistory.length > maxHistoryLength) {
+      searchHistory.splice(0, searchHistory.length - maxHistoryLength);
+    }
+    chrome.storage.local.set({searchHistory: searchHistory});
+    updateHistoryDiv();
+  }
+}
+
+function setHistoryVisibility(makeVisible) {
+  document.getElementById('history').style.display = makeVisible ? 'block' : 'none';
+  document.getElementById('show-history').title = makeVisible ? HIDE_HISTORY_TITLE : SHOW_HISTORY_TITLE;
+  if(makeVisible) {
+    document.getElementById('show-history').className = 'selected';
+  } else {
+    document.getElementById('show-history').className = '';
+  }
+}
+
+function setCaseInsensitiveElement() {
+  var caseInsensitive = chrome.storage.local.get({'caseInsensitive':DEFAULT_CASE_INSENSITIVE},
+  function (result) {
+    document.getElementById('insensitive').title = result.caseInsensitive ? DISABLE_CASE_INSENSITIVE_TITLE : ENABLE_CASE_INSENSITIVE_TITLE;
+    if(result.caseInsensitive) {
+      document.getElementById('insensitive').className = 'selected';
+    } else {
+      document.getElementById('insensitive').className = '';
+    }
+  });
+
+}
+function toggleCaseInsensitive() {
+  var caseInsensitive = document.getElementById('insensitive').className == 'selected';
+  document.getElementById('insensitive').title = caseInsensitive ? ENABLE_CASE_INSENSITIVE_TITLE : DISABLE_CASE_INSENSITIVE_TITLE;
+  if(caseInsensitive) {
+    document.getElementById('insensitive').className = '';
+  } else {
+    document.getElementById('insensitive').className = 'selected';
+  }
+  sentInput = false;
+  chrome.storage.local.set({caseInsensitive: !caseInsensitive});
+  passInputToContentScript(true);
+}
+
+function clearSearchHistory() {
+  searchHistory = [];
+  chrome.storage.local.set({searchHistory: searchHistory});
+  updateHistoryDiv();
+}
+
+
+/*** LISTENERS ***/
+document.getElementById('next').addEventListener('click', function() {
+  selectNext();
+});
+
+document.getElementById('prev').addEventListener('click', function() {
+  selectPrev();
+});
+
+document.getElementById('clear').addEventListener('click', function() {
+  sentInput = false;
+  document.getElementById('inputRegex').value = '';
+  passInputToContentScript();
+  document.getElementById('inputRegex').focus();
+});
+
+document.getElementById('show-history').addEventListener('click', function() {
+  var makeVisible = document.getElementById('history').style.display == 'none';
+  setHistoryVisibility(makeVisible);
+  chrome.storage.local.set({isSearchHistoryVisible: makeVisible});
+});
+
+document.getElementById('insensitive').addEventListener('click', function() {
+  toggleCaseInsensitive();
+});
+
+document.getElementById('copy-to-clipboard').addEventListener('click', function () {
+  chrome.tabs.query({
+      'active': true,
+      'currentWindow': true
+    },
+    function (tabs) {
+      if ('undefined' != typeof tabs[0].id && tabs[0].id) {
+        chrome.tabs.sendMessage(tabs[0].id, {
+          'message': 'copyToClipboard'
+        });
+      }
+    });
+});
+
+/* Received returnSearchInfo message, populate popup UI */ 
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if ('returnSearchInfo' == request.message) {
+    processingKey = false;
+    if (request.numResults > 0) {
+      document.getElementById('numResults').textContent = String(request.currentSelection+1) + ' of ' + String(request.numResults);
+    } else {
+      document.getElementById('numResults').textContent = String(request.currentSelection) + ' of ' + String(request.numResults);
+    }
+    if (!sentInput) {
+      document.getElementById('inputRegex').value = request.regexString;
+    }
+    if (request.numResults > 0 && request.cause == 'selectNode') {
+      addToHistory(request.regexString);
+    }
+    if (request.regexString !== document.getElementById('inputRegex').value) {
+      passInputToContentScript();
+    }
+  }
+});
+
+/* Key listener for selectNext and selectPrev
+ * Thanks a lot to Cristy from StackOverflow for this AWESOME solution
+ * http://stackoverflow.com/questions/5203407/javascript-multiple-keys-pressed-at-once */
+var map = [];
+onkeydown = onkeyup = function(e) {
+    map[e.keyCode] = e.type == 'keydown';
+    if (document.getElementById('inputRegex') === document.activeElement) { //input element is in focus
+      if (!map[16] && map[13]) { //ENTER
+        if (sentInput) {
+          selectNext();
+        } else {
+          passInputToContentScript();
+        }
+      } else if (map[16] && map[13]) { //SHIFT + ENTER
+        selectPrev();
+      }
+    }
+}
+/*** LISTENERS ***/
+
+/*** INIT ***/
+/* Retrieve from storage whether we should use instant results or not */
+chrome.storage.local.get({
+    'instantResults' : DEFAULT_INSTANT_RESULTS,
+    'maxHistoryLength' : MAX_HISTORY_LENGTH,
+    'searchHistory' : null,
+    'isSearchHistoryVisible' : false},
+  function(result) {
+    if(result.instantResults) {
+      document.getElementById('inputRegex').addEventListener('input', function() {
+        passInputToContentScript();
+      });
+    } else {
+      document.getElementById('inputRegex').addEventListener('change', function() {
+        passInputToContentScript();
+      });
+    }
+    console.log(result);
+    if(result.maxHistoryLength) {
+      maxHistoryLength = result.maxHistoryLength;
+    }
+    if(result.searchHistory) {
+      searchHistory = result.searchHistory.slice(0);
+    } else {
+      searchHistory = [];
+    }
+    setHistoryVisibility(result.isSearchHistoryVisible);
+    updateHistoryDiv();
+  }
+);
+
+/* Get search info if there is any */
+chrome.tabs.query({
+  'active': true,
+  'currentWindow': true
+},
+function(tabs) {
+  if ('undefined' != typeof tabs[0].id && tabs[0].id) {
+    chrome.tabs.sendMessage(tabs[0].id, {
+      'message' : 'getSearchInfo'
+    }, function(response){
+      if (response) {
+        // Content script is active
+        console.log(response);
+      } else {
+        console.log(response);
+        document.getElementById('error').textContent = ERROR_TEXT;
+      }
+    });
+  }
+});
+
+/* Focus onto input form */
+document.getElementById('inputRegex').focus();
+window.setTimeout( 
+  function(){document.getElementById('inputRegex').select();}, 0);
+//Thanks to http://stackoverflow.com/questions/480735#comment40578284_14573552
+
+var makeVisible = document.getElementById('history').style.display == 'none';
+setHistoryVisibility(makeVisible);
+chrome.storage.local.set({isSearchHistoryVisible: makeVisible});
+
+setCaseInsensitiveElement();
+/*** INIT ***/
+
